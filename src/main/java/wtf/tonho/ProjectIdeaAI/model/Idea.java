0ab17cbb824bb810206e0cea698560cce563bb30
Tonho <antonioreverso.com@gmail.com>
2025-04-16 17:29:44 -0300

    feat: Relacionamento entre tabelas

--- src/main/java/wtf/tonho/ProjectIdeaAI/model/Idea.java
+++ src/main/java/wtf/tonho/ProjectIdeaAI/model/Idea.java
@@ -1,4 +1,39 @@
 package wtf.tonho.ProjectIdeaAI.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import wtf.tonho.ProjectIdeaAI.model.enums.Categoria;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Table(name = "idea")
+@NoArgsConstructor
+@AllArgsConstructor
 public class Idea {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String descricao;
+    @Enumerated(EnumType.STRING)
+    private Categoria categoria;
+
+    @ManyToMany
+    @JoinTable(
+            name = "idea_tecnologia",
+            joinColumns = @JoinColumn(name = "idea_id"),
+            inverseJoinColumns = @JoinColumn(name = "tecnologia_id")
+    )
+    private List<Tecnologia> tecnologias;
+    private LocalDateTime dataCriacao;
+    @PrePersist
+    public void prePersist() {
+        this.dataCriacao = LocalDateTime.now();
+    }
+
+
 }
--- src/main/java/wtf/tonho/ProjectIdeaAI/model/Tecnologia.java
+++ src/main/java/wtf/tonho/ProjectIdeaAI/model/Tecnologia.java
@@ -1,4 +1,30 @@
 package wtf.tonho.ProjectIdeaAI.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import wtf.tonho.ProjectIdeaAI.model.enums.TipoTecnologia;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "tecnologia")
+@NoArgsConstructor
+@AllArgsConstructor
 public class Tecnologia {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    @Enumerated(EnumType.STRING)
+    private TipoTecnologia tipo;
+
+    @ManyToMany(mappedBy = "tecnologias")
+    private List<Idea> ideias = new ArrayList<>();
+
+
 }
