168ed5310eef9a515dc55d4da4a597264b69496b
Tonho <antonioreverso.com@gmail.com>
2025-04-17 21:26:25 -0300

    feat: Envia post para api do gpt

--- src/main/java/wtf/tonho/ProjectIdeaAI/controller/ProjectIdeaController.java
+++ src/main/java/wtf/tonho/ProjectIdeaAI/controller/ProjectIdeaController.java
@@ -1,4 +1,23 @@
 package wtf.tonho.ProjectIdeaAI.controller;
 
-public class ProjectController {
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+import wtf.tonho.ProjectIdeaAI.service.ChatGptService;
+
+@RestController
+public class ProjectIdeaController {
+
+    private ChatGptService chatGptService;
+
+    public ProjectIdeaController(ChatGptService chatGptService) {
+        this.chatGptService = chatGptService;
+    }
+
+    @GetMapping("/generate")
+    public Mono<ResponseEntity<String>> generateProject() {
+        return chatGptService.generateProject().map(project -> ResponseEntity.ok(project)).defaultIfEmpty(ResponseEntity.notFound().build());
+    }
 }
